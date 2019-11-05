ce120683dd505f30ca74b6d44239b8d30622808f
VladDesktop <vlad.neri@gmail.com>
2019-11-04 16:45:07 -0800

    initial commit

---
+++ app/components/Template/Nav.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+import data from '../../data/contact';
+
+const Nav = () => (
+  <section id="sidebar">
+    <section id="intro">
+      <Link to="/" className="logo">
+        <img src={`${BASE_PATH}/images/me_icon.jpg`} alt="" />
+      </Link>
+      <header>
+        <h2>Vladimir Neri</h2>
+        <p><a href="info@vladimirneri.com">info@vladimirneri.com</a></p>
+      </header>
+    </section>
+
+    <section className="blurb">
+      <h2>About</h2>
+      <p>Hi, I&apos;m Vlad. I’m a Full Stack Web Developer with a background in commercial photography, 
+        and life-long dedication to learning.   Effective at combining creativity and problem solving to develop user-friendly applications. 
+        Known among staff for strong wit and attention to detail no matter the complexity of the project.   
+
+      </p>
+      <ul className="actions">
+        <li>
+          {window.location.pathname !== `${BASE_PATH}/resume` ? <Link to="/resume" className="button">Learn More</Link> : <Link to="/about" className="button">About Me</Link>}
+        </li>
+      </ul>
+    </section>
+
+    <section id="footer">
+      <ul className="icons">
+        {data.map((s) => (
+          <li key={s.label}>
+            <a href={s.link}>
+              <FontAwesomeIcon icon={s.icon} />
+            </a>
+          </li>
+        ))}
+      </ul>
+      <p className="copyright">&copy; Vladimir Neri <Link to="/">iamvladneri.com</Link>.</p>
+    </section>
+  </section>
+);
+
+export default Nav;
